843df2f49bd0d2c9b8e002fae988b7509f80ee93
elaemmer <elaemmer@f5520b42-8414-0410-9ae7-91ee0cd12377>
2009-05-20 15:37:36 +0000

    git-svn-id: https://subversion.renater.fr/audiovideocours/trunk/server@597 f5520b42-8414-0410-9ae7-91ee0cd12377

--- audiovideocours/univ-r_av/WebContent/scripts/convertAll2Mp4.sh
+++ audiovideocours/univ-r_av/WebContent/scripts/convertAll2Mp4.sh
@@ -5,6 +5,6 @@
 # Third argument: the output video name (without extension)
 
 cd $1
-nice -n 9 /usr/bin/ffmpeg -i $2 -acodec aac -ab 64k -vcodec h264 -b 1200k -s 1280x720 -y $3.mp4
-#nice -n 9 /usr/bin/ffmpeg -i $2 -acodec libfaac -ab 64k -vcodec libx264 -b 1200k -s 1280x720 -y $3.mp4
+#nice -n 9 /usr/bin/ffmpeg -i $2 -acodec aac -ab 64k -vcodec h264 -b 1200k -s 1280x720 -y $3.mp4
+nice -n 9 /usr/bin/ffmpeg -i $2 -acodec libfaac -ab 64k -vcodec libx264 -b 1200k -s 1280x720 -y $3.mp4
 #nice -n 9 /usr/bin/ffmpeg -i $2 -acodec aac -ab 128k -vcodec h264 -b 3000k -s 1920x1080 -y $3.mp4
