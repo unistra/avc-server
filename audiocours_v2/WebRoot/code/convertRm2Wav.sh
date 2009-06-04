5ccb9f11f729738fb4242bc220880f50c0896a25
elaemmer <elaemmer@f5520b42-8414-0410-9ae7-91ee0cd12377>
2009-06-04 08:34:55 +0000

    git-svn-id: https://subversion.renater.fr/audiovideocours/trunk/server@632 f5520b42-8414-0410-9ae7-91ee0cd12377

--- audiocours_v2/WebRoot/code/convertRm2Wav.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script qui reencode un fichier video wmv en rm
-# Prend en argument le repertoire du fichier video
-cd $1
-mplayer "enregistrement-video.rm" -ao "pcm:file=enregistrement-micro.wav" -vc "dummy" -vo "null"
-touch test2
--- audiocours_v2/WebRoot/code/mp3cl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x ./mp3cleaner
-./mp3cleaner $1 $2
