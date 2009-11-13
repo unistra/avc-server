b054f1aca2daffa04241a70f3270fbd8dd3ee016
elaemmer <elaemmer@f5520b42-8414-0410-9ae7-91ee0cd12377>
2009-11-13 10:22:36 +0000

    git-svn-id: https://subversion.renater.fr/audiovideocours/trunk/server@823 f5520b42-8414-0410-9ae7-91ee0cd12377

--- univ-r_av_temp/WebContent/files/js/confirmation.js
+++
@@ -1,5 +0,0 @@
-/* Function which displays a confirmation message */
-function confirmation( message, urlOK) {
-	if (confirm(message))
-		window.location = urlOK;
-}
--- univ-r_av_temp/WebContent/scripts/mail.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#script which sends an email to confirm the reception of a new course
-# First argument: the message
-# Second argument: the subject
-# Third argument: the e-mail adress
-
-echo "$1"|mail -s "$2" -a "From: no-reply@unistra.fr" $3
