8745fb3ee95246c92ccf5351b887fc2ff4ea435c
morganbohn <morganbohn@f5520b42-8414-0410-9ae7-91ee0cd12377>
2013-03-06 14:46:16 +0000

    add webm for video upload and additional doc
    git-svn-id: https://subversion.renater.fr/audiovideocours/trunk/server/univ-r_av@1092 f5520b42-8414-0410-9ae7-91ee0cd12377

--- WebContent/scripts/jobs_encodage/JobEnc.sh
+++ WebContent/scripts/jobs_encodage/JobEnc.sh
@@ -186,6 +186,8 @@ bash $PTHSCR/convertAll2Mp4.sh $MediaFolder $ORI$CourseID.$Extension $CourseID $
 /usr/bin/AtomicParsley $MediaFolder/"$CourseID"_ipod.mp4 --title "$TITLE" --artist "$AUTHOR" --year "$DATE" --album "$FORMATION" --comment "$COMMENT" --overWrite &> /dev/null
 # ogv for html5
 ffmpeg2theora $MediaFolder/"$CourseID".mp4 -o $MediaFolder/"$CourseID".ogv &> /dev/null
+# webm for html5
+bash $PTHSCR/convertAll2Webm.sh $MediaFolder $CourseID mp4
 ;;
 
 ADDV)
@@ -204,6 +206,9 @@ ffmpeg2theora $MediaFolder/$FOLDER/"$AV$CourseID".mp4 -o $MediaFolder/$FOLDER/"$
 # remove original file
 rm $MediaFolder/$FOLDER/$ORI$AV$CourseID.$Extension
 rm $MediaFolder/$FOLDER/$AV$CourseID.mp3
+# webm for html5
+bash $PTHSCR/convertAll2Webm.sh $MediaFolder/$FOLDER $AV$CourseID mp4
+;;
 esac
 
 # Maj du mediatype sur le serveur avc
